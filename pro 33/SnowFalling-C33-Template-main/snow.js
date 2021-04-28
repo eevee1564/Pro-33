f8db09678cbd4821d72023e95fb5ea66048a610f
eevee1564 <78247125+eevee1564@users.noreply.github.com>
2021-04-28 17:37:54 +0530

    Add files via upload

---
+++ pro 33/SnowFalling-C33-Template-main/snow.js
@@ -0,0 +1,14 @@
+class snow{
+
+    constructor(bodyA, pointB){
+        var options={
+            restitution:0.8,
+            density:1.4,
+            friction:0.3
+        }
+    }
+    
+    boy=loadImage("snow3.jpg");
+    boy=velocityY(5);
+
+}
